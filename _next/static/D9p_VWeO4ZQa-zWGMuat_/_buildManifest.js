5635574699cab0767d709844b047f9e76e992010
shuvoooo <shuvo.punam@gmail.com>
2021-03-22 11:30:22 +0600

    1st commit

---
+++ _next/static/D9p_VWeO4ZQa-zWGMuat_/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST = {__rewrites:[],"/":["static\u002Fchunks\u002Fpages\u002Findex-85e4a676866b9f26398f.js"],"/_error":["static\u002Fchunks\u002Fpages\u002F_error-30b38936249109f3f301.js"],sortedPages:["\u002F","\u002F_app","\u002F_error"]};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
